d7f8f853eefa9122537f2f817027f06c345d3e18
CyanForWeb <95006417+CyanForWeb@users.noreply.github.com>
2021-12-14 17:23:20 +0900

    Update main.js
    日本語var

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,10 +7,16 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は、とても暑かった。 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+//const storyText = 'その日は、とても暑かった。 so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+//const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+//const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+//const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+const storyText = 'その日は、とても暑かった。:insertx:は散歩に出掛けた。:inserty:に着いた。:insertz:体重:insertW:の:insertx:は驚いたが、Bobは驚かなかった。:insertY:ではよくあることだった。';
+const insertX = ['スヌーピー', 'ミッキーマウス', 'サンタクロース'];
+const insertY = ['ディズニーランド', '千駄ヶ谷', '津田塾大学'];
+const insertZ = ['混雑していた。', '雪が降っていた。'];
+const insertW = ['50kg','110ポンド'];
 
 randomize.addEventListener('click', result);
 
